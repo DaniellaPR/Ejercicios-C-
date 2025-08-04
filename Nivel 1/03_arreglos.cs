64f5736d122793190f44db7cab23a744390239c7
DaniellaPR <159065844+DaniellaPR@users.noreply.github.com>
2025-08-03 21:08:23 -0500

    Create 03_arreglos.cs

---
+++ Nivel 1/03_arreglos.cs
@@ -0,0 +1,26 @@
+//Escribir un programa que tenga una función que reciba como parámetros 2 arreglos
+//de una dimensión de enteros y entregue como resultado lo mismo que el operador +
+//para las cadenas, es decir, los 2 arreglos conectados en un areglo resultante.
+//Use su función en un programa que le pida ingresar los 2 arreglos al usuario y
+//que le presente la fusión de esos 2 arreglos.
+
+using System;
+
+namespace Clase2Arreglos
+{
+    internal class Program
+    {
+        static int[] Fusion(int[] a; int[] b)
+        {
+            int[] r = null;
+            return r;
+        }
+        static void Main(string[] args)
+        {
+        int[] a1 = { 1, 2, 3, 4 };
+        int[] a2 = { 1, 345 };
+        int[] f;
+        f = Fusion(a1, a2);
+        }
+    }
+}

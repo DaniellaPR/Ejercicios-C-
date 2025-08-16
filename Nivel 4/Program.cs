eb2a5d45d518480df7fc110e11703ccf979c8488
Daniela Pozo <159065844+DaniellaPR@users.noreply.github.com>
2025-08-16 15:37:15 -0500

    Add files via upload

---
+++ Nivel 4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursividad
+{
+    class Program
+    {
+        public static int Multi(int n, int m)
+        {
+            int respuesta=0;
+            if (m == 1)
+                respuesta =n;
+            else
+                respuesta = n + Multi(n, m -1);
+            return respuesta;
+        }
+        static void Main(string[] args)
+        {
+            int x;
+            x = Multi(4, 5);
+        }
+    }
+}

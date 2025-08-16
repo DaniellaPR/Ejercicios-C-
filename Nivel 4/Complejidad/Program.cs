aeaa8d941818d9c25d7905c86e680353546851c8
Daniela Pozo <159065844+DaniellaPR@users.noreply.github.com>
2025-08-16 15:36:33 -0500

    Add files via upload

--- Nivel 4/Complejidad/Program.cs
+++ Nivel 4/Complejidad/Program.cs
@@ -1,26 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace Recursividad
 {
     class Program
     {
-        public int BBinaria(int num, int[] arr)
+        public static int Multi(int n, int m)
         {
-            int lon = arr.Length;
-            int mitad = lon / 2;
-            if (num > arr[mitad])
-                arr[mitad] = lon;
-            if (num < arr[mitad + 1])
-                arr[mitad + 1] = lon;
+            int respuesta=0;
+            if (m == 1)
+                respuesta =n;
             else
-                return mitad;
-            for (int i = 0; i<lon; i++)
-                Console.Write(arr[i]);
+                respuesta = n + Multi(n, m -1);
+            return respuesta;
         }
         static void Main(string[] args)
         {
-            int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
-            
+            int x;
+            x = Multi(4, 5);
         }
     }
 }
